c6f237eeddb3f411b837e78dd5d0b0b04b065f35
renanwilson <renan.alves.ra16@gmail.com>
2022-11-24 13:54:55 -0300

    fix: remove unused fragment and add better conditional

--- src/Components/Controller.tsx
+++ src/Components/Controller.tsx
@@ -1,10 +1,10 @@
 import { useAppcontext } from "../Context/AppContext";
 
 import { ApiData } from "./pages/ApiData/Index";
-import { Index } from "./pages/Index";
+import { Index } from "./pages/Index/Index";
 
 export default function Controller() {
   const { apiCalled } = useAppcontext();
-  console.log(apiCalled);
-  return <>{apiCalled === 0 ? <Index /> : <ApiData />}</>;
+
+  return apiCalled ? <ApiData /> : <Index />;
 }
