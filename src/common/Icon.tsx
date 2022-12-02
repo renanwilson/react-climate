2866f0daf1fb5fe60f132745763c5b8cf43c3a5b
renanwilson <renan.alves.ra16@gmail.com>
2022-12-02 12:10:33 -0300

    feat: adjust Icons to data api

--- src/common/Icon.tsx
+++ src/common/Icon.tsx
@@ -1,20 +1,13 @@
-import { BsFillEmojiSunglassesFill } from "react-icons/bs";
 import { useAppcontext } from "../Context/AppContext";
-
-const types = (temperatura: number) => {
-  const Icones = (): JSX.Element => {
-    return (
-      <>
-        <>{temperatura < 20 && <BsFillEmojiSunglassesFill />}</>
-      </>
-    );
-  };
-  return <Icones />;
-};
 
 export function IconTypes() {
   const { weather } = useAppcontext();
-  const Icons = types(Number(weather.main.temp));
-
-  return Icons;
+  return (
+    <img
+      src={`icons/${weather.weather[0].icon}.png`}
+      alt="Icon"
+      width="70px"
+      height="70px"
+    />
+  );
 }
