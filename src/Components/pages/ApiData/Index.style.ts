6228f2c5a219b2ef583f0326a74a416b17796388
renanwilson <renan.alves.ra16@gmail.com>
2022-12-02 12:11:56 -0300

    feat: adjust page to best perform in mobile

--- src/Components/pages/ApiData/Index.style.ts
+++ src/Components/pages/ApiData/Index.style.ts
@@ -1,35 +1,26 @@
+import { Grid, Typography } from "@mui/material";
 import styled from "styled-components";
+import { IconTypes } from "../../../common/Icon";
 
-export const Container = styled.div`
+export const Container = styled(Grid)`
+  display: flex;
   width: 100%;
   height: 100%;
-  display: flex;
   align-items: center;
   justify-content: center;
-  flex-direction: column;
+  padding: 12px;
 `;
-export const Card = styled.div`
-  width: 50%;
-  height: 75%;
-  display: flex;
-  align-items: center;
-  box-shadow: 0px 2px 4px rgb(25 31 35 / 24%);
-  flex-direction: column;
-  border: 1px solid;
-  border-radius: 12px;
-  padding: 20px;
-`;
-export const Place = styled.div`
-  width: 50%;
-  justify-content: space-around;
-  margin-top: 10px;
-  display: flex;
-  align-items: center;
+export const Card = styled(Grid)`
+  padding: 12px;
   box-shadow: 0px 2px 4px rgb(25 31 35 / 24%);
   border: 1px solid;
   border-radius: 12px;
-  padding: 20px;
 `;
+export const IconTemp = styled(IconTypes)`
+  width: 0px;
+  height: 0px;
+`;
+export const Place = styled(Typography)``;
 export const Information = styled.div`
   width: 100%;
   display: flex;
--- src/Components/pages/ApiData/Index.tsx
+++ src/Components/pages/ApiData/Index.tsx
@@ -1,44 +1,41 @@
-import { Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import React from "react";
 
-import {
-  Container,
-  Card,
-  Place,
-  Information,
-  Temperatura,
-  Nuvems,
-  Imagem,
-} from "./Index.style";
-import { IconTypes } from "../../../common/Icon";
+import { Container, Card, IconTemp } from "./Index.style";
 
 import { useAppcontext } from "../../../Context/AppContext";
 
 export function ApiData() {
   const { weather } = useAppcontext();
+  let pressure = Number(weather.main.pressure) / 100;
 
   return (
-    <>
-      <Container>
-        <Card>
-          <Place>
-            <Typography variant="h3">{weather.name}</Typography>
-          </Place>
+    <Container container direction="column">
+      <Card item xs={12} lg={2}>
+        <Grid
+          item
+          xs={12}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          direction="row"
+        >
+          <Typography variant="h4">{weather.name}</Typography>
+          <IconTemp />
+        </Grid>
+        <Grid item xs={12} alignItems="center" justifyContent="center">
+          <Typography variant="h5"> Min: {weather.main.temp_min}°C</Typography>
+          <Typography variant="h5"> Atual: {weather.main.temp}°C</Typography>
+          <Typography variant="h5"> Max: {weather.main.temp_max}°C</Typography>
+        </Grid>
 
-          <Typography>{weather.weather[0].description}</Typography>
-          <Imagem src={`icons/${weather.weather[0].icon}.png`} alt="" />
-
-          <Information>
-            <Temperatura>
-              <IconTypes />
-              <Typography variant="h4">{weather.main.temp}°C</Typography>
-            </Temperatura>
-            <Nuvems>
-              <Typography variant="h4">{weather.main.temp}°C</Typography>
-            </Nuvems>
-          </Information>
-        </Card>
-      </Container>
-    </>
+        <Typography variant="h5">
+          Humidade: {weather.main.humidity}°C
+        </Typography>
+        <Typography variant="h5">Pressão: {pressure}%</Typography>
+      </Card>
+    </Container>
   );
 }
+
+// TODO: ADJUST STYLES
