e7da269fc4e0f1c2607d6d2248f33d3200a30b52
renanwilson <renan.alves.ra16@gmail.com>
2023-02-03 19:32:16 -0300

    fix: remove icon

--- src/Components/Icons/Icons.styles.ts
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const IconTemp = styled.img`
-  width: 100px;
-  height: 100px;
-`;
--- src/Components/Icons/Icons.tsx
+++
@@ -1,7 +0,0 @@
-import { useWeatherContext } from "Context/WeatherContext";
-import { IconTemp } from "./Icons.styles";
-
-export function IconTypes() {
-  const { weather } = useWeatherContext();
-  return <IconTemp src={`icons/${weather.weather[0].icon}.png`} alt="Icon" />;
-}
