83a0eec811e46c2e0ea2add31261a6ab42232547
renanwilson <renan.alves.ra16@gmail.com>
2023-02-03 19:34:39 -0300

    feat: add search input component

---
+++ src/Components/SearchInput/styles.ts
@@ -0,0 +1,11 @@
+import { AiOutlineSearch } from "react-icons/ai";
+import styled from "styled-components";
+import { Input } from "@mui/material";
+
+export const SearchIcon = styled(AiOutlineSearch)`
+  color: #ffff;
+  margin: 10px;
+`;
+export const InputColor = styled(Input)`
+  color: white !important;
+`;
