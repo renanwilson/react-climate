433a9ec36f65244a9117e76349392ca00397071a
renanwilson <renan.alves.ra16@gmail.com>
2023-02-06 12:54:11 -0300

    fix: add key to content list

--- src/Components/WeatherDetails/ContentList/ContentList.tsx
+++ src/Components/WeatherDetails/ContentList/ContentList.tsx
@@ -4,11 +4,11 @@ export function ContentList() {
   const { Info } = Content();
   return (
     <>
-      {Info.map((info) => {
+      {Info.map(({ title, info }) => {
         return (
-          <Details>
-            <DetailsFont variant="inherit">{info.title}</DetailsFont>
-            <DetailsFont variant="inherit">{info.info} %</DetailsFont>
+          <Details key={title}>
+            <DetailsFont variant="inherit">{title}</DetailsFont>
+            <DetailsFont variant="inherit">{info} %</DetailsFont>
           </Details>
         );
       })}
