622d69f6eb6c92695d1f6859aea1adea44234d08
renanwilson <renan.alves.ra16@gmail.com>
2023-02-06 14:34:43 -0300

    fix: endpoint of api

--- src/services/WeatherApi.tsx
+++ src/services/WeatherApi.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const WeatherApi = axios.create({
-  baseURL: "http://api.openweathermap.org/data/2.5/weather?",
+  baseURL: "https://api.openweathermap.org/data/2.5/weather?",
   params: {
     appid: process.env.REACT_APP_OPEN_WHEATER_KEY,
     lang: "pt_br",
